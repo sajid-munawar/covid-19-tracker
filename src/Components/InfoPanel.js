b588583bc50b4b1c7ed42487a9713f036dd9d083
sajid507 <sajidmunawar666@gmail.com>
2020-11-10 21:41:31 +0500

    Update InfoPanel.js

--- src/Components/InfoPanel.js
+++ src/Components/InfoPanel.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import CountUp from 'react-countup';
+// import CountUp from 'react-countup';
 
 const useStyles = makeStyles((theme) => ({
     root: {
