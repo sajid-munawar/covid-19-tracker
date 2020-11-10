251dc92efb0a044bccad5daee317b910411ea37d
sajid507 <sajidmunawar666@gmail.com>
2020-11-10 21:34:20 +0500

    updated grid and panel

---
+++ src/Components/Countries.js
@@ -0,0 +1,13 @@
+// import { useEffect } from "react";
+// import React from "./React";
+
+// function Country(){
+//     useEffect(()=>{
+//         async function getData(){
+//             const response=await fetch ("https://disease.sh/v3/covid-19/countries")
+//             const data=await data.json
+//             console.log("country= ",data)
+//         }
+//         getData();
+//     })
+// }
--- src/Components/InfoPanel.js
+++ src/Components/InfoPanel.js
@@ -11,15 +11,29 @@ const useStyles = makeStyles((theme) => ({
         marginTop: 50,
     },
     paper: {
+        height:"450px",
         padding: theme.spacing(2),
         textAlign: 'center',
         color: theme.palette.text.secondary,
+        // backgroundColor:"rgb(0,0,255,0.5)"
     },
     title : {
         // color:"rgb(0,0,255,0.5)",
         color: "#3f51b5",
         textTransform : "capitalize"
         // textTransform : "uppercase"
+    },
+    total : {
+        backgroundColor:"rgb(0,0,255,0.5)"
+    },
+    recoverd : {
+        backgroundColor:"rgb(0,255,0,0.5)"
+    },
+    deaths : {
+        backgroundColor:"rgb(255,0,0,0.5)"
+    },
+    h3 : {
+        color:"white"
     }
 }));
 // function getCase(str){
@@ -37,6 +51,7 @@ export default function InfoPanel() {
         setGlobalData(data)
         setData(false);
         console.log(data);
+       
       }
       getData();
     },[])
@@ -46,11 +61,35 @@ export default function InfoPanel() {
             <h1>Loading data....</h1>
         </div>
     }
+    // console.log(globalData[Object.keys(globalData[0])])
+    // console.log(Object.keys(globalData)[1])
+   
 
     return (
         <div className={classes.root}>
             <Grid container spacing={3}>
-            {Object.keys(globalData).map((key,ind)=>{
+                <Grid item xs={12} sm={4} >
+                <Paper className={classes.paper} elevation={3} className={classes.total}>
+                    <h3>Total Cases</h3>
+                    <h3>{Object.values(globalData)[1]}</h3>
+                    </Paper>
+                </Grid>
+                <Grid item xs={12} sm={4} >
+                <Paper className={classes.paper} elevation={3} className={classes.deaths}> 
+                    <h3>Deaths</h3>
+                    <h3>{Object.values(globalData)[3]}</h3>
+                    </Paper>
+                </Grid>
+                <Grid item xs={12} sm={4} >
+                <Paper className={classes.paper} elevation={3} className={classes.recoverd}>
+                    <h3>Recovered</h3>
+                    <h3>{Object.values(globalData)[5]}</h3>
+                    </Paper>
+                </Grid>
+
+
+
+            {/* {Object.keys(globalData).map((key,ind)=>{
                
                 return (
                     <Grid item xs={12} sm={4} key={ind}>
@@ -65,7 +104,7 @@ export default function InfoPanel() {
                         </Paper>
                     </Grid>
                 )
-            })}
+            })} */}
             </Grid>
             
             
