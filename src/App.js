4b9d293fd43a33b068264b597b9db04c72356e89
sajid-munawar <sajidmunawar666@gmail.com>
2020-11-11 09:27:20 +0500

    added imgae

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import {NavBar , InfoPanel} from "./Components"
+import covid from "./covid.jpg";
 
 
 function App() {
   return (
     <div>
        <NavBar/>
+       <img src={covid} alt="covid-19" align={"center"} width={400} />
         <InfoPanel/>
     </div>
   );
